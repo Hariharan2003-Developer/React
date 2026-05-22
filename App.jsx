df1ab7a586055c56969127c9edb449323b7cd29e
Hariharan2003-Developer <hariharanit302@gmail.com>
2026-05-22 16:06:59 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./counterSlice";
+
+function App() {
+  const count = useSelector(state => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => dispatch(increment())}>
+        Increment
+      </button>
+      <button onClick={() => dispatch(decrement())}>
+        Decrement
+      </button>
+    </div>
+  );
+}
+
+export default App;
